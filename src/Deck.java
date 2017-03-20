13362eb2d1e71c7b8d3ea7202f928a0cdfd72364
Anwar Saleeby <anwar.saleeby@gmail.com>
2017-03-20 16:22:13 -0400

    Hand is original, some fixes to Deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,48 +1,46 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * Created by matthew on 3/15/17.
- */
-public class Deck {
-  public final String[] suites = {"hearts","spades","diamonds","clubs"};
-  ArrayList<Card> deck;
-
-  public Deck(){
-    shuffleNewDeck();
-  }
-
-  /**
-   * shuffles through collections
-   */
-  public void shuffle(){
-    Collections.shuffle(deck);
-  }
-
-  /**
-   * creates a new deck in order
-   */
-  public void newDeck(){
-    this.deck = new ArrayList<Card>();
-
-    for (String suite: suites) {
-      for (int value=1;value<14;value++){
-        this.deck.add(new Card(value,suite));
-      }
-    }
-  }
-
-  public void shuffleNewDeck(){
-    newDeck();
-    shuffle();
-  }
-
-  public ArrayList<Card> getDeck(){
-
-    return deck;
-  }
-
-  public Card getCard(int index){
-    return deck.get(index);
-  }
+public class Deck 
+{
+	public final String[] suites = {"hearts", "spades", "diamonds", "clubs"};
+	ArrayList<Card> deck;
+	public Deck()
+	{
+		shuffleNewDeck();
+		
+	}
+	
+	public void shuffle()
+	{
+		Collections.shuffle(deck);
+		
+	}
+	
+	public void newDeck()
+	{
+		deck = new ArrayList<Card>();
+		for(int value = 1; value < 14; value++)
+		{
+			for(String suite: suites)
+			{
+				deck.add(new Card(value, suite));
+			}
+		}
+	}
+	
+	public void shuffleNewDeck()
+	{
+		newDeck();
+		shuffle();
+	}
+	public ArrayList<Card> getDeck()
+	{
+		return getDeck();
+	}
+	
+	public Card getCard(int index)
+	{
+		return deck.get(index);
+	}
 }
---
+++ src/Hand.java
@@ -0,0 +1,65 @@
+import java.util.Vector;
+import java.util.Random;
+
+public class Hand
+
+{
+	private Vector<Card> hand;
+
+	public Hand()
+	{
+		hand = new Vector<Card>();
+	}
+
+	public void addCard(Card c)
+	{
+
+		hand.add(c);
+	}
+
+	public void displayCardsinHand()
+	{
+		for (int card = 0; card < hand.size(); card++)
+		{
+			System.out.println(hand.elementAt(card));
+		}
+	}
+
+	public int getCardsinHand()
+	{
+		return hand.size();
+	}
+
+	public Card getCard(int position)
+	{
+		if(position >= 0 && position < hand.size())
+			return (Card)hand.elementAt(position);
+		else
+			return null;
+	} 
+
+	public int getScore()
+	{
+		int value = 0;
+		boolean ace = false;
+
+		for (int i = 0; i < hand.size(); i++)
+		{
+			Card c;
+			c = getCard(i);
+			value = value + c.getRankValue();
+
+			if(c.getRankValue() == 1)
+			{
+				ace = true;
+			}
+		}
+
+		if(ace == true && value + 10 <= 21)
+		{
+			value = value + 10;
+		}
+
+		return value;   
+	}
+}
