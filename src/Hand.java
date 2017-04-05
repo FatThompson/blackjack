5889254d8c621da29bf3375f59da5718f442a2f4
FatThompson <matthewbiggiethompson@gmail.com>
2017-04-05 13:37:59 -0400

    A large refactoring of the Hand Class

--- src/Hand.java
+++ src/Hand.java
@@ -3,34 +3,111 @@ import java.util.ArrayList;
 public class Hand
 
 {
-	private ArrayList<Card> cards;
+  private ArrayList<Card> nonAceCards;
+  private ArrayList<Card> aceCards;
+  private int totalValue;
+  private boolean bustedHand;
 
-	public Hand() {
-		cards = new ArrayList<Card>();
-	}
+
+  public Hand() {
+    nonAceCards = new ArrayList<Card>();
+    aceCards = new ArrayList<Card>();
+    totalValue=0;
+    bustedHand=false;
+  }
 
   /**
    * add a card to the hand
    * @param c
    */
-	public void addCard(Card c)
-	{
-    cards.add(c);
-	}
+  public void addCard(Card card)
+  {
+    //add an ace
+    if(card.getValue()==1 || card.getValue()==11){
+      totalValue++;
+      if(!(10+totalValue > 21)) {
+        totalValue+=10;
+        card.setValue(11);
+      }
+      aceCards.add(card);
+    }
+    //add a non ace
+    else{
+      nonAceCards.add(card);
+      totalValue+=card.value;			
+    }
+    //update the busting
+    updateBust();
+  }
 
+  /**
+   * updates the current isbustValue
+   * @return
+   */
+  public boolean updateBust(){
+    //if bust and there are aces
+    if(checkbust(totalValue) && aceCards.size()>0){
+      for (Card card : aceCards) {
+        //iterate and find any high aces
+        if(checkbust(totalValue-10) && card.getValue()==11 ){
+          card.setValue(1);
+          totalValue-=10;
+        }
+      }
+    }    
+    //return the result
+    return checkbust(totalValue);
+  }
+  
+  /**
+   * Checks value is greater than 21;
+   * @param value
+   * @return
+   */
+  protected boolean checkbust(int value){
+    //value based on blackjack rules
+    return value>21;    
+  }
+  
   /**
    * returns the amount of cards in the hand
    * @return
    */
-	public int size() {
-		return cards.size();
-	}
-
-	public ArrayList<Card> getCards(){
-	  return cards;
+  public int numberOfCards() {
+    return nonAceCards.size()+aceCards.size();
   }
 
-
-
-
+  /**
+   * returns an ArrayList of cards in the current hand
+   * @return
+   */
+  public ArrayList<Card> cards(){
+    ArrayList<Card> cards = new ArrayList<Card>();
+    
+    for (Card nonace : nonAceCards) {
+      cards.add((Card) nonace);
+    }for (Card ace : aceCards) {
+      cards.add((Card) ace);
+    }
+    return cards;
+  }
+  
+  @Override
+  public String toString(){
+    String hand = "Value: " + totalValue + "\tand Cards: ";
+    for (Card card : cards()) {
+      hand+=card.toString(); 
+    }
+    
+    return hand;    
+  }
 }
+
+
+
+
+
+
+
+
+
--- tst/HandTest.java
+++ tst/HandTest.java
@@ -1,12 +1,101 @@
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
  * Created by matthew on 3/29/17.
  */
 public class HandTest {
+ 
+
+  /**
+   * check addCard method
+   */
   @Test
-  public void testAddCard(){
+  public void addCardTest(){
+    Hand hand = new Hand();
+    Card ace = new Card(1, "Spades");
+    Card nonAce1 = new Card(9, "Spades");
+    Card nonAce2 = new Card(8, "Heart");
+    Card nonAce3 = new Card(7, "Diamonds");
+    
+    Assertions.assertEquals(0, hand.numberOfCards());
+    hand.addCard(nonAce1);
+    Assertions.assertEquals(1, hand.numberOfCards());
+    hand.addCard(nonAce2);
+    Assertions.assertEquals(2, hand.numberOfCards());
+    hand.addCard(nonAce3);
+    Assertions.assertEquals(3, hand.numberOfCards());
+    
+  }
+  
 
+  /**
+   * check updateBustTest method
+   */
+  @Test
+  public void updateBustTest(){
+     Hand hand = new Hand();
+     Card ace = new Card(1, "Spades");
+     Card nonAce1 = new Card(9, "Spades");
+     Card nonAce2 = new Card(8, "Heart");
+     Card nonAce3 = new Card(7, "Diamonds");
+     
+     Assertions.assertEquals(false, hand.updateBust());
+     
+     hand.addCard(ace);
+     Assertions.assertEquals(false, hand.updateBust());
 
+     hand.addCard(nonAce1);
+     Assertions.assertEquals(false, hand.updateBust());
+
+     hand.addCard(nonAce2);
+     Assertions.assertEquals(false, hand.updateBust());
+
+     hand.addCard(nonAce3);
+     Assertions.assertEquals(true, hand.updateBust());
+     
+  }
+
+  /**
+   * check numberOfCardsTest method
+   */
+  @Test
+  public void numberOfCardsTest(){
+    Hand hand = new Hand();
+    Card ace = new Card(1, "Spades");
+    Card nonAce = new Card(13, "Spades");
+
+    Assertions.assertEquals(0, hand.numberOfCards());
+    
+    hand.addCard(ace);
+    Assertions.assertEquals(1, hand.numberOfCards());
+    
+    hand.addCard(nonAce);
+    Assertions.assertEquals(2, hand.numberOfCards());
+  }
+
+  /**
+   * check cardsTest method
+   */
+  @Test
+  public void cardsTest(){
+    Hand hand = new Hand();
+    Card ace = new Card(1, "Spades");
+    Card nonAce = new Card(13, "Spades");
+    
+    hand.addCard(ace);
+    hand.addCard(nonAce);
+    
+    ArrayList<Card> returnstatement = new ArrayList<Card>();
+    returnstatement.add(nonAce);
+    returnstatement.add(ace);
+    
+    Assertions.assertEquals(returnstatement, hand.cards());
+    
   }
 }
+
+
+
